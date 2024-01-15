b82202fd2a76d8dc847f1b8537b483171daa2ea9
Kiran Rana <kiranrana8973@gmail.com>
2024-01-15 18:48:44 +0545

    Added pull to refresh

--- lib/screen/comment_view.dart
+++ lib/screen/comment_view.dart
@@ -21,56 +21,63 @@ class _CommentViewState extends ConsumerState<CommentView> {
   @override
   Widget build(BuildContext context) {
     final state = ref.watch(commentViewModelProvider);
-    return Scaffold(
-      backgroundColor: Colors.amber[100],
-      appBar: AppBar(
-        backgroundColor: Colors.yellow[400],
-        title: const Text('Pagination Flutter'),
-        actions: [
-          TextButton.icon(
-            onPressed: () {
-              ref.read(commentViewModelProvider.notifier).resetState();
-            },
-            icon: const Icon(Icons.refresh),
-            label: const Text('Refresh'),
-          ),
-        ],
-      ),
-      body: NotificationListener(
-        onNotification: (notification) {
-          if (notification is ScrollEndNotification) {
-            // Scroll garda feri last ma ho ki haina bhanera check garne ani data call garne
-            if (_scrollController.position.extentAfter == 0) {
-              // Data fetch gara api bata
-              ref.read(commentViewModelProvider.notifier).getComments();
-            }
+    return NotificationListener(
+      onNotification: (notification) {
+        if (notification is ScrollEndNotification) {
+          // Scroll garda feri last ma ho ki haina bhanera check garne ani data call garne
+          if (_scrollController.position.extentAfter == 0) {
+            // Data fetch gara api bata
+            ref.read(commentViewModelProvider.notifier).getComments();
           }
-          return true;
-        },
-        child: Column(
-          children: [
-            Expanded(
-              child: ListView.separated(
-                separatorBuilder: (context, index) => const Divider(),
-                controller: _scrollController,
-                itemCount: state.comments.length,
-                physics: const AlwaysScrollableScrollPhysics(),
-                itemBuilder: (context, index) {
-                  final comment = state.comments[index];
-                  return ListTile(
-                    leading: Text(comment.id.toString()),
-                    title: Text(comment.name),
-                    subtitle: Text(comment.email),
-                  );
-                },
-              ),
+        }
+        return true;
+      },
+      child: Scaffold(
+        backgroundColor: Colors.amber[100],
+        appBar: AppBar(
+          backgroundColor: Colors.yellow[400],
+          title: const Text('Pagination Flutter'),
+          actions: [
+            TextButton.icon(
+              onPressed: () {
+                ref.read(commentViewModelProvider.notifier).resetState();
+              },
+              icon: const Icon(Icons.refresh),
+              label: const Text('Refresh'),
             ),
-            // Data load huda feri progress bar dekhaune natra banda garne
-            if (state.isLoading)
-              const CircularProgressIndicator(color: Colors.red),
-
-            const SizedBox(height: 10),
           ],
+        ),
+        body: RefreshIndicator(
+          // Yo chai pull to refresh ko lagi ho
+          color: Colors.red,
+          onRefresh: () async {
+            ref.read(commentViewModelProvider.notifier).resetState();
+          },
+          child: Column(
+            children: [
+              Expanded(
+                child: ListView.separated(
+                  separatorBuilder: (context, index) => const Divider(),
+                  controller: _scrollController,
+                  itemCount: state.comments.length,
+                  physics: const AlwaysScrollableScrollPhysics(),
+                  itemBuilder: (context, index) {
+                    final comment = state.comments[index];
+                    return ListTile(
+                      leading: Text(comment.id.toString()),
+                      title: Text(comment.name),
+                      subtitle: Text(comment.email),
+                    );
+                  },
+                ),
+              ),
+              // Data load huda feri progress bar dekhaune natra banda garne
+              if (state.isLoading)
+                const CircularProgressIndicator(color: Colors.red),
+
+              const SizedBox(height: 10),
+            ],
+          ),
         ),
       ),
     );
